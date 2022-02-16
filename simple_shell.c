3cd715f46f253a7a2fac3cfc607710ce39e1fb10
Abhishek Aryal <abhishekaryal@wireless-nat-inside.uiowa.edu>
2022-02-16 10:31:40 -0600

    simpleshell works

--- simple_shell.c
+++ simple_shell.c
@@ -2,44 +2,54 @@
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
-#include<sys/types.h>
 #include<sys/wait.h>
 #include<readline/readline.h>
-#include<readline/history.h>
 
+#define MAXINPUT 1024
 
-#define MAXINPUT = 1024; 
-
-void greet(){
-    printf("* * * * * * * * * * \n");
-    printf("*                  *\n");
-    printf("*    CS3620 HW2    *\n");
-    printf("*   Simple Shell   *\n");
-    printf("*                  *\n");
-    printf(" * * * * * * * * * *\n");
-}
-
-// input method
-int getInput(char *str){
-    char *buf; 
-
-	buf = readline("\n>>> ");
-
-    if(strlen(buf) != 0){
-        add_history(buf); 
-        strcpy(str, buf);
-        return 0; 
-    }else{
-        return 1; 
-    }
-}
-
+int getInput(char *input);
+void execute(char **args);
+void removeSpace(char* input, char **final);
 
 int main(){
     char inputString[1024];
-    greet();
+    char *finalString[1024];
+
+    printf("*** CS 3620 HW2 Simple Terminal ***\n");
     while(1){
-        printf("TMPDIR=%s\n", getenv("TMPDIR")); // print directory
-        if(!getInput(inputString)) break; 
+        if(getInput(inputString))continue;
+
+        removeSpace(inputString, finalString);
+
+        if(strcmp(finalString[0], "quit") == 0){
+            printf("bye\n");
+            exit(0);
+        }        
+        execute(finalString);
+        
     }
+}
+void removeSpace(char *input, char **final){
+    for(int i = 0; i < MAXINPUT; i++){
+        final[i] = strsep(&input, " ");
+        if(final[i] == NULL) break; 
+        if(strlen(final[i]) == 0) i--;
+    }
+}
+void execute(char **args){
+    int pid = fork(); 
+
+    if(pid < 0) printf("fork uncessful\n");
+    else if(pid != 0) wait(NULL);
+    else{
+        int exec = execvp(args[0], args);
+        if(exec < 0) printf("unable to execute\n");
+        exit(0);
+    }
+}
+int getInput(char *input){
+    char *tempInput = readline(">--> ");
+    if(strlen(tempInput) == 0) return 1; 
+    strcpy(input, tempInput); 
+    return 0; 
 }
